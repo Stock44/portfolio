38cd81c248181cc4f7519e59c102f61e81e06d65
Hiram Muñoz <hirammunozr@outlook.com>
2025-05-18 23:19:31 -0600

    Add Prettier config and replace twJoin with static classes
    Introduced a Prettier configuration file to standardize code formatting across the project. Additionally, removed the use of `twJoin` in the layout file and replaced it with static Tailwind CSS class definitions for simplicity.

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+	jsxSingleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+};
+
+export default config;
